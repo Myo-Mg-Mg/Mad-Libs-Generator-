983aef3d676a0b5b90f2872c0ecc45d28667f92f
Myo-Mg-Mg <116092866+Myo-Mg-Mg@users.noreply.github.com>
2022-10-18 22:40:52 +0630

    Add files via upload

---
+++ Mad Libs Generator.py
@@ -0,0 +1,22 @@
+trap = 1
+while (trap <5):
+# All the questions that the program asks the user
+    name = input("Please,Enter your name:")
+    actor_name = input("Please show of your favorite an actor:")
+    hobby = input("Input your beloved hobby:")
+    food = input("Point out your disliked food:")
+    place = input("Write a place that you want to visit: ")
+
+# Displays the story based on the users input
+    print ("------------------------------------------")
+    print ("Luckily",name,"have a change to go",place,"with your favorite actor",
+    actor_name,"who loves",hobby)
+    print ("But",actor_name,"is eating",food,"unfortunately in the same air line seats with you")
+    print ("As well as",name,"saw this manner & you will feed",food,"from him")
+    print ("If you don't have a bit,you will be annoyed from",actor_name)
+    print ("How do u solve this problem, Poor-",name)
+    print ()
+    print ("This is the end story of ",name,",")
+    print ("You've got a happy experience.")
+    print ("------------------------------------------")
+    trap = trap + 1
